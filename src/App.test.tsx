d18e120267faee8eb47d384f3622d975984821b8
Shannon Largman <shannon.largman@gmail.com>
2022-08-30 13:43:07 -0700

    feat: add functions for string manipulation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  checkDecimal,
+  checkNegative,
+  countDigits,
+  formatOperand,
+} from './lib/utilities';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('utilities', () => {
+  describe('countDigits', () => {
+    it('should count digits correctly', () => {
+      expect(countDigits('1000')).toEqual(4);
+      expect(countDigits('1,000')).toEqual(4);
+      expect(countDigits('-1,000')).toEqual(4);
+      expect(countDigits('-1,000.0')).toEqual(5);
+    });
+  });
+
+  describe('checkDecimal', () => {
+    it('should check decimals correctly', () => {
+      expect(checkDecimal('-1,000')).toBe(false);
+      expect(checkDecimal('-1,000.10')).toBe(true);
+      expect(checkDecimal('1,000.88')).toBe(true);
+    });
+  });
+
+  describe('checkNegative', () => {
+    it('should check decimals correctly', () => {
+      expect(checkNegative('1,000')).toBe(false);
+      expect(checkNegative('1,000.88')).toBe(false);
+      expect(checkNegative('-1,000')).toBe(true);
+      expect(checkNegative('-1,000.10')).toBe(true);
+    });
+  });
+
+  describe('formatOperand', () => {
+    it('should format operands correctly', () => {
+      expect(formatOperand('008')).toBe('8');
+      expect(formatOperand('008.0')).toBe('8.0');
+      expect(formatOperand('-8.')).toBe('-8.');
+      expect(formatOperand('-8.0')).toBe('-8.0');
+      expect(formatOperand('123456789')).toBe('123,456,789');
+      expect(formatOperand('123456789.')).toBe('123,456,789');
+      expect(formatOperand('123456789.0')).toBe('123,456,789');
+      expect(formatOperand('123.4567890')).toBe('123.456789');
+    });
+  });
 });
---
+++ src/lib/utilities.ts
@@ -0,0 +1,119 @@
+const MAX_INPUT_LENGTH = 9;
+
+export function countDigits(operand: string) {
+  const digitRegex = new RegExp('\\d');
+  const filteredDigits = operand
+    .split('')
+    .filter((char) => digitRegex.test(char));
+  return filteredDigits.length;
+}
+
+export function checkDecimal(operand: string) {
+  return operand.includes('.');
+}
+
+export function checkNegative(operand: string) {
+  return operand[0] === '-';
+}
+
+function filterCommas(operand: string) {
+  return operand
+    .split('')
+    .filter((char) => char !== ',')
+    .join('');
+}
+
+function addCommas(integer: string) {
+  let withCommas = '';
+
+  for (let i = integer.length - 1; i >= 0; i--) {
+    const j = integer.length - 1 - i;
+    if (j === 3 || j === 6) {
+      withCommas = ',' + withCommas;
+    }
+    withCommas = integer[i] + withCommas;
+  }
+
+  return withCommas;
+}
+
+function trimLeadingZeros(integer: string) {
+  let trimmed = '';
+  let nonZeroDigitFound = false;
+
+  for (let i = 0; i < integer.length; i++) {
+    const digit = integer[i];
+    if (digit !== '0') nonZeroDigitFound = true;
+    if (nonZeroDigitFound) trimmed = trimmed + digit;
+  }
+
+  return trimmed === '' ? '0' : trimmed;
+}
+
+export function formatOperand(operand: string) {
+  operand = filterCommas(operand);
+  const number = Number(operand);
+
+  if (
+    Math.abs(number) >= 10 ** MAX_INPUT_LENGTH ||
+    (Math.abs(number) <= 10 ** -(MAX_INPUT_LENGTH - 1) && number !== 0)
+  ) {
+    return 'Error';
+  }
+
+  const operandIsNegative = checkNegative(operand);
+
+  const prefix = operandIsNegative ? '-' : '';
+
+  const operandParts = operand.split(/\D/);
+  const integerPart = operandIsNegative ? operandParts[1] : operandParts[0];
+  const fractionPart = operandIsNegative ? operandParts[2] : operandParts[1];
+
+  const operandHasDecimal = checkDecimal(operand);
+  // don't add decimal to max length integers
+  const decimal =
+    operandHasDecimal && integerPart.length < MAX_INPUT_LENGTH ? '.' : '';
+
+  const fraction =
+    fractionPart === undefined
+      ? ''
+      : fractionPart.slice(0, MAX_INPUT_LENGTH - integerPart.length);
+
+  let integer = trimLeadingZeros(integerPart);
+  integer = addCommas(integer);
+
+  return prefix + integer + decimal + fraction;
+}
+
+export function calculateDisplaySize(operand: string) {
+  const digits = countDigits(operand);
+  if (digits <= 6) return 50;
+  return 50 - 4 * (digits - 6);
+}
+
+export function evaluate(operand1: string, operator: string, operand2: string) {
+  const number1 = Number(filterCommas(operand1));
+  const number2 = Number(filterCommas(operand2));
+  let result;
+
+  if (operator === '/') {
+    result = number1 / number2;
+  } else if (operator === '*') {
+    result = number1 * number2;
+  } else if (operator === '-') {
+    result = number1 - number2;
+  } else if (operator === '+') {
+    result = number1 + number2;
+  }
+
+  if (
+    result === undefined ||
+    isNaN(result) ||
+    result === Infinity ||
+    result === -Infinity
+  ) {
+    return 'Error';
+  }
+
+  return formatOperand(result.toString());
+}
