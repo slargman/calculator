2bac79550dd5091f39a664f00265c3f2fa7f7266
Shannon Largman <shannon.largman@gmail.com>
2022-08-30 14:36:01 -0700

    feat: add reducer function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import {
   MAX_INPUT_LENGTH,
   countDigits,
@@ -10,18 +10,115 @@ import {
 } from './lib/utilities';
 import './App.css';
 
-export default function App() {
-  const [mem, setMem] = useState<null | string>(null);
-  const [operand, setOperand] = useState<string>('0');
-  const [operator, setOperator] = useState<null | string>(null);
+type State = {
+  mem: null | string;
+  operand: string;
+  operator: null | string;
+};
 
-  const handleClearClick = () => {};
+type Action =
+  | { type: 'cleared_all' }
+  | { type: 'cleared_operand' }
+  | { type: 'appended_digit'; digit: string }
+  | { type: 'appended_decimal' }
+  | { type: 'applied_plus_minus' }
+  | { type: 'applied_percent' }
+  | { type: 'selected_operator'; operator: string }
+  | { type: 'evaluated_with_operator'; operator: string }
+  | { type: 'evaluated_with_equals' }
+  | { type: 'input_new_number'; digit: string };
+
+const initialState = {
+  mem: null,
+  operand: '0',
+  operator: null,
+};
+
+function reducer(state: State, action: Action) {
+  const { mem, operand, operator } = state;
+  switch (action.type) {
+    case 'cleared_all': {
+      return initialState;
+    }
+    case 'cleared_operand': {
+      return { ...state, operand: '0' };
+    }
+    case 'appended_digit': {
+      return {
+        ...state,
+        operand: formatOperand(operand + action.digit),
+      };
+    }
+    case 'appended_decimal': {
+      return {
+        ...state,
+        operand: formatOperand(operand + '.'),
+      };
+    }
+    case 'applied_plus_minus': {
+      return {
+        ...state,
+        operand: formatOperand(
+          checkNegative(operand) ? operand.substring(1) : '-' + operand
+        ),
+      };
+    }
+    case 'applied_percent': {
+      return {
+        ...state,
+        operand: formatOperand(evaluate(operand, '/', '100')),
+      };
+    }
+    case 'selected_operator': {
+      return {
+        ...state,
+        operator: action.operator,
+      };
+    }
+    case 'evaluated_with_operator': {
+      return {
+        mem: evaluate(mem, operator, operand),
+        operator: action.operator,
+        operand: '0',
+      };
+    }
+    case 'evaluated_with_equals': {
+      return {
+        mem: null,
+        operator: null,
+        operand: evaluate(mem, operator, operand),
+      };
+    }
+    case 'input_new_number': {
+      return {
+        mem: null,
+        operator: null,
+        operand: formatOperand(action.digit),
+      };
+    }
+  }
+  throw Error('Unknown action: ' + action.type);
+}
+
+export default function App() {
+  const [{ mem, operand, operator }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const handleClearClick = () => {
+    if (operand === '0') {
+      dispatch({ type: 'cleared_all' });
+    } else {
+      dispatch({ type: 'cleared_operand' });
+    }
+  };
   const handlePlusMinusClick = () => {};
   const handlePercentClick = () => {};
   const handleOperatorClick = (op: string) => {};
   const handleEqualsClick = () => {};
   const handleDecimalClick = () => {};
-  const handleNumberClick = (number: string) => {};
+  const handleDigitClick = (digit: string) => {};
 
   const operators = [
     { op: '/', gridArea: 'division', display: '÷' },
@@ -48,7 +145,7 @@ export default function App() {
           className="Button Function"
           onClick={handleClearClick}
         >
-          {operand === '' ? 'AC' : 'C'}
+          {operand === '0' ? 'AC' : 'C'}
         </button>
         <button
           onClick={handlePlusMinusClick}
@@ -93,7 +190,7 @@ export default function App() {
             key={i}
             data-number={i.toString()}
             onClick={() => {
-              handleNumberClick(`${i}`);
+              handleDigitClick(`${i}`);
             }}
             style={{ gridArea: `number-${i}` }}
             className={'Button ' + (i === 0 ? 'Button-0' : '')}
